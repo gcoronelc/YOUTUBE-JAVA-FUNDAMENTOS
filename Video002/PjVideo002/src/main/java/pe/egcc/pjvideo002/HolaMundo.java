97f9677c20c92a3ccefe7c1b6ed5ea45b542ebac
Gustavo Coronel <gcoronelc@gmail.com>
2020-08-25 08:09:19 -0500

    Video 002

---
+++ Video002/PjVideo002/src/main/java/pe/egcc/pjvideo002/HolaMundo.java
@@ -0,0 +1,19 @@
+package pe.egcc.pjvideo002;
+
+/**
+ * @author Eric Gustavo Coronel Castillo
+ * @blog www.desarrollasoftware.com
+ * @email gcoronelc@gmail.com
+ * @youtube www.youtube.com/c/DesarrollaSoftware
+ * @facebook www.facebook.com/groups/desarrollasoftware/
+ */
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola mundo.");
+		System.out.println("Hola Gustavo Coronel.");
+		
+	}
+	
+}
